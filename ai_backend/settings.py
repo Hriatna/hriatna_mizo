b68c01f3dd7689fc1cf634a2b9b0cd1666770483
Hriatna <hriatna.ai@gmail.com>
2023-02-24 22:20:28 +0530

    Database update

--- ai_backend/settings.py
+++ ai_backend/settings.py
@@ -124,13 +124,22 @@ WSGI_APPLICATION = 'ai_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'HriatnaAI$HriatnaAI$default',
+        'USER': 'HriatnaAI',
+        'PASSWORD': 'Thufingte123456',
+        'HOST': 'HriatnaAI.mysql.pythonanywhere-services.com',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
